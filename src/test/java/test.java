dc98157c817ef48255cbe10e0282dd74e58b5db8
daniel <gokhancimenlik@gmail.com>
2026-06-11 09:24:36 -0400

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,10 +2,7 @@ public class test {
     static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is the projec version 1");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+
     }
     }
-// done with version done
+// done with version three
