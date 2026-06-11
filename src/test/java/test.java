560efc280b1edd6ec2be52c777f806e0bf2e61b5
daniel <gokhancimenlik@gmail.com>
2026-06-11 09:08:00 -0400

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("this is the projec version 1");
+    }
+    }
+// done with version done
