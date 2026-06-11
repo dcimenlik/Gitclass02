17ed2af95a846756c256b12418c6d32495d7705d
daniel <gokhancimenlik@gmail.com>
2026-06-11 09:20:40 -0400

    second commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,6 +2,10 @@ public class test {
     static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is the projec version 1");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
     }
 // done with version done
