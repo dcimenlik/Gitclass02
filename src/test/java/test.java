543f8f90ead1d698fb8922d99e2093ead30d1fdf
daniel <gokhancimenlik@gmail.com>
2026-06-15 16:54:45 -0400

    fifth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,8 @@
 public class test {
     static void main(String[] args) {
 
-        System.out.println("this is the projec version 1");
+        System.out.println("this is the projec version 5");
 
     }
     }
-// done with version four
+// done with version five
